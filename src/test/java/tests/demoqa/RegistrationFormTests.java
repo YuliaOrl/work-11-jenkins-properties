aecb77781529c6f3d9b8ba3848b33298ae546a70
YuliaOrl <orbitalik32@gmail.com>
2022-09-12 18:58:51 +0300

    Added test properties_1

--- src/test/java/tests/demoqa/RegistrationFormTests.java
+++ src/test/java/tests/demoqa/RegistrationFormTests.java
@@ -10,7 +10,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
-public class RegistrationFormTests extends TestBase_2 {
+public class RegistrationFormTests extends TestBase_1 {
 
     @Test
     void fillFormTest () {
---
+++ src/test/java/tests/demoqa/TestBase_1.java
@@ -0,0 +1,42 @@
+package tests.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestBase_1 {
+
+    @BeforeAll
+    static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.baseUrl = "https://demoqa.com";
+
+        Configuration.browser = System.getProperty("browser_name", "chrome");
+        Configuration.browserVersion = System.getProperty("browser_version", "100.0");
+        Configuration.browserSize = System.getProperty("browser_size", "1500x800");
+
+        if (System.getProperty("remote") != null) {
+            Configuration.remote = System.getProperty("remote");
+        }
+
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        if (System.getProperty("remote") != null) {
+            Attach.addVideo();
+        }
+    }
+}
